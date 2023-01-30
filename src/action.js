a6cff4819b19f6f8900e5daf2582534abc34ad1b
Shazeen Mahmood <mahmood@talon.one>
2023-01-30 14:22:55 +0100

    updating index.js again

--- src/action.js
+++ src/action.js
@@ -1,9 +1,9 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 async function run() {
-  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
-  const octokit = github.getOctokit(GITHUB_TOKEN)
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const octokit = github.getOctokit(GITHUB_TOKEN);
 
   const { context = {} } = github;
   const { pull_request } = context.payload;
@@ -12,7 +12,7 @@ async function run() {
     ...context.repo,
     issue_number: pull_request.number,
     body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
-  })
+  });
 }
 
 run();
