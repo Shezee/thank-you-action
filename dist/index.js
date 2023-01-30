281df4534d9e3f6742bd7e9f957113f39d52709a
Shazeen Mahmood <mahmood@talon.one>
2023-01-30 15:46:00 +0100

    forgot to build

--- dist/index.js
+++ dist/index.js
@@ -9781,11 +9781,13 @@ const octokit = github.getOctokit(GITHUB_TOKEN);
 const { pull_request } = context.payload;
 
 async function run() {
-await octokit.rest.issues.createComment({
-	...context.repo,
-	issue_number: pull_request.number,
-	body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
-});
+  console.log('Thank you for creating this PR!');
+
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+  });
 }
 
 run();
